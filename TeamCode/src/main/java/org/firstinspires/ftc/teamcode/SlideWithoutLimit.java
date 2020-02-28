9dfd316fe2b72dfe71c18d0ab0a9df8a88f8c55b
JacobGowan99bacon <45701922+JacobGowan99bacon@users.noreply.github.com>
2020-02-28 13:16:35 -0500

    Added an opmode that makes only the slide go without the limit

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SlideWithoutLimit.java
@@ -0,0 +1,27 @@
+package org.firstinspires.ftc.teamcode;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotor.ZeroPowerBehavior;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+@com.qualcomm.robotcore.eventloop.opmode.TeleOp(name="SlideNoLimit", group="TeleOp")
+public class SlideWithoutLimit<opModeIsActive> extends LinearOpMode {
+    @Override
+    public void runOpMode() {
+        //slide motors
+        DcMotor slide_left = hardwareMap.get(DcMotor.class, "slide_left");
+        DcMotor slide_right = hardwareMap.get(DcMotor.class, "slide_right");
+        slide_right.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        waitForStart();
+        while (opModeIsActive()) {
+            double slidePow = -gamepad2.left_stick_y;
+            slide_left.setPower(slidePow);
+            slide_right.setPower(slidePow);
+
+
+        }
+    }
+}
